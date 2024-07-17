1898a35db82839ba0de302cff7bf2e7730f61365
aldo <aldo@MacBook-Air-di-aldo.local>
2024-07-17 09:51:20 +0200

    fix .gitignore

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 22:08:22 by aldo              #+#    #+#             */
+/*   Updated: 2024/07/17 09:30:23 by aldo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+
+void	ft_print_numbers_double_digit(char n)
+{
+	char nt;
+
+	nt = (n % 10) + 48;
+	n = (n / 10) + 48;
+	ft_putchar(n);
+	ft_putchar(nt);
+	n++;
+}
+
+void	ft_print_comb2(void)
+{
+	char	n1;
+	char	n2;
+	
+	n1 = 0;
+	n2 = 1;
+	while (n1 <= 98)
+	{
+		while (n2 <= 99)
+		{
+			ft_print_numbers_double_digit(n1);
+			write(1, " ", 1);
+			ft_print_numbers_double_digit(n2);
+			write(1, ", ", 2);
+			n2++;
+		}
+		n1++;
+		n2 = n1 + 1;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
