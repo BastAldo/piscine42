0a070bcb5a24d07aa42ef6226b9dea035bdfd88f
Aldo Panzano <aldpanza@e3r5p7.42roma.it>
2024-07-17 20:53:00 +0200

    create todo

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,12 +6,12 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 20:33:50 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/17 20:34:40 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/17 20:52:40 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+//TODO: scorrere la stringa fino a fine stringa \0
 void ft_putstr(char *str)
 {
 	
