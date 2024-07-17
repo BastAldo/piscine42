93e199804d65afeed12447cf4ea2d463c8353964
Aldo <aldo-panzano@hotmail.it>
2024-07-17 23:37:20 +0200

    fix gitignore

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -3,21 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 20:33:50 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/17 20:52:40 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/17 23:17:34 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 //TODO: scorrere la stringa fino a fine stringa \0
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, str[i], 1);
+		i++;
+	}
 }
 
 int	main()
 {
-
+	char	stri[4] = "Ciao"; 
 }
