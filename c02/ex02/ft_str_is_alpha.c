eb9e45c1ea24365f47c9ada9dd0f7fbc40080e9a
Aldo <aldo-panzano@hotmail.it>
2024-07-18 17:57:10 +0200

    add ex 02

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 16:58:08 by aldo              #+#    #+#             */
+/*   Updated: 2024/07/18 17:56:39 by aldo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] && i < ft_strlen(str))
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z')))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
