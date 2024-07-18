a5b9425d542d2c9ba8c7bf9d1bf1c39117baed92
Aldo <aldo-panzano@hotmail.it>
2024-07-18 16:47:45 +0200

    add ex01

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 15:56:25 by aldo              #+#    #+#             */
+/*   Updated: 2024/07/18 16:47:10 by aldo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (n && src[i] && i < n)
+	{
+		if (i >= ft_strlen(src))
+			dest[i] = 0;
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
