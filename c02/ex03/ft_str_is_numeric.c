5919def5ddf6a8ccdc41cc626c0da19c0c431eb4
Aldo Panzano <aldpanza@e3r4p1.42roma.it>
2024-07-18 19:16:06 +0200

    add ex03

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 18:52:55 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/18 19:14:46 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_str_is_numeric(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] && i < ft_strlen(str))
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	s[] = "";
+	int	i;
+
+	i = ft_str_is_numeric(s);
+	return (i);
+}
