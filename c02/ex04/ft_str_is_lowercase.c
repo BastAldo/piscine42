538717700fee3966e680e848c59ce3b7b1d61035
Aldo Panzano <aldpanza@e3r4p1.42roma.it>
2024-07-18 19:24:22 +0200

    add ex04

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 19:13:15 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/18 19:22:27 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_str_is_lowercase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] && i < ft_strlen(str))
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
