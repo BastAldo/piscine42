3a4c01424a2375dc805a1291e512fae25a7f798e
Aldo <aldo-panzano@hotmail.it>
2024-07-18 10:21:03 +0200

    add ex07 and fix norm issue

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,25 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/18 09:30:21 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/18 10:20:06 by aldo             ###   ########.fr       */
+/*   Created: 2024/07/18 09:50:26 by aldo              #+#    #+#             */
+/*   Updated: 2024/07/18 10:19:17 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+void	ft_swap(int *a, int *b)
+{
+	int	t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void	ft_rev_int_tab(int *tab, int size)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (i < size / 2)
 	{
+		ft_swap(&tab[i], &tab[size - i - 1]);
 		i++;
 	}
-	return (i);
 }
