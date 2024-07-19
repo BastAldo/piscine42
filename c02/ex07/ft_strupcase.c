0802fc0c9c403054fb644e3bf66cba00e03b5c4e
Aldo Panzano <aldpanza@e3r4p1.42roma.it>
2024-07-19 10:45:48 +0200

    fix norm issue on ex07

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -6,12 +6,12 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 00:39:36 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/19 10:43:35 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/19 10:45:30 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//TODO: finire funzione che trasforma lettere in maiuscolo
+
 char	*ft_strupcase(char *str)
 {
 	unsigned int	i;
