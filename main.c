de82b04470091f7d5bb8ad6d0de3097cbd7511a5
Aldo <aldo-panzano@hotmail.it>
2024-07-20 00:07:55 +0200

    add ex09 skeletron and create todo

--- main.c
+++ main.c
@@ -6,20 +6,20 @@
 /*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 08:32:09 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/19 22:48:37 by aldo             ###   ########.fr       */
+/*   Updated: 2024/07/19 23:55:56 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strupcase(char *str);
+char	*ft_strlowcase(char *str);
 
 int	main(void)
 {
-	char	str[] = "aldo Panzano!";
+	char	str[] = "Aldo Panzano!";
 	char	*i;
 	
-	i = ft_strupcase(str);
+	i = ft_strlowcase(str);
 	printf("return %s", i);
 	return (0);
 }
