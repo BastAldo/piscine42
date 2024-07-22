088c303787db50555c71f13f7b1b9255aff36229
Aldo <aldo-panzano@hotmail.it>
2024-07-22 20:06:19 +0200

    add c03/ex00 skeleton

--- main.c
+++ main.c
@@ -3,23 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 09:03:10 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/22 11:25:09 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/22 19:59:04 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strcapitalize(char *str);
+int	ft_strcmp(char *s1, char *s2);
 
 int	main(void)
 {
-	char	str[50] = "aLd=o p1anZanO";
-	char	*i;
-	
-	i = ft_strcapitalize(str);
-	printf("return %s", i);
+	int	i;
+
+	i = ft_strcmp("abc", "ab");
+	printf("return %i", i);
 	return (0);
 }
