8d1c6fa2f6f4fb0f3fab4b6bb591df339a0e8af7
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-22 18:44:00 +0200

    fix norm

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,14 +6,14 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:08:12 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/22 15:09:31 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/22 17:49:59 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int i;
-	unsigned int y;
+	unsigned int	i;
+	unsigned int	y;
 
 	i = 0;
 	y = 0;
