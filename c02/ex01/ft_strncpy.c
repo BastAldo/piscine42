d19c1a5c214210c0f8bdbc5971b76c2f9f443b79
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-22 17:01:04 +0200

    fix norm issue

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,11 +6,9 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 09:02:36 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/22 16:59:52 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/22 17:00:51 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
@@ -22,22 +20,10 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-
 	while (i < n)
 	{
 		dest[i] = 0;
 		i++;
 	}
-	
 	return (dest);
-}
-
-int	main(void)
-{
-char	dest[50];
-char	*dest2;
-char	src[] = "aldo";
-dest2 = ft_strncpy(dest, src, 8);
-write(1, &dest2, 1);
-return (0);
 }
