e382d095763bbdde67344dd4f39948338908c55b
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-22 15:13:44 +0200

    fix ex06: printable charactare are 32 up to 126

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -6,7 +6,7 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 09:04:08 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/22 09:04:10 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/22 15:13:02 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_str_is_printable(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (!(str[i] >= 33 && str[i] <= 126))
+		if (!(str[i] >= 32 && str[i] <= 126))
 		{
 			return (0);
 		}
