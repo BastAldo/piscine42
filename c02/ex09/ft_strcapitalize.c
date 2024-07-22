3c122bf31b56ae1e9e660b88c4fbcbb8da143ec7
Aldo Panzano <aldpanza@e3r5p7.42roma.it>
2024-07-22 12:13:53 +0200

    add ex 09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -3,22 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/19 23:57:02 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/20 00:06:18 by aldo             ###   ########.fr       */
+/*   Created: 2024/07/22 09:04:45 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/22 12:13:17 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//TODO: bisogna splittare str usando gli spazi
+
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
-	
-	i = 0;
-	while(str[i])
+	unsigned int	i;
+
+	if (str[0] != '\0' && str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	i = 1;
+	while (str[i] != '\0')
 	{
-		if (str ==)
+		if (((str[i] >= 'a' && str[i] <= 'z')
+				|| (str[i] >= 'A' && str[i] <= 'Z'))
+			&& (str[i - 1] < '0'
+				|| (str[i - 1] > '9' && str[i - 1] < 'A')
+				|| (str[i - 1] > 'Z' && str[i - 1] < 'a')
+				|| str[i - 1] > 'z'))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		++i;
 	}
+	return (str);
 }
--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/19 08:32:09 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/19 23:55:56 by aldo             ###   ########.fr       */
+/*   Created: 2024/07/22 09:03:10 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/22 11:25:09 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strlowcase(char *str);
+char	*ft_strcapitalize(char *str);
 
 int	main(void)
 {
-	char	str[] = "Aldo Panzano!";
+	char	str[50] = "aLd=o p1anZanO";
 	char	*i;
 	
-	i = ft_strlowcase(str);
+	i = ft_strcapitalize(str);
 	printf("return %s", i);
 	return (0);
 }
