25d91f8bd8fdefcdb82bbef96dc76d4b80680ebc
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-23 12:12:02 +0200

    c03 ex00 to ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 11:38:54 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/23 12:09:24 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	y;
+
+	i = 0;
+	y = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[y] && y < nb)
+	{
+		dest[i + y] = src[y];
+		y++;
+	}
+	dest[i + y] = 0;
+	return dest;
+}
