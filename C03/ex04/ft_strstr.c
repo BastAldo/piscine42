94e7d65d4ead96c136f7b6ca9126aaa07ac9eb1c
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-23 15:17:56 +0200

    add ex04

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,11 +6,29 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:10:36 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/23 12:11:05 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/23 15:17:22 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
+	int	i;
+	int	y;
+
+	i = 0;
+	y = 0;
+	if (!(to_find[i]))
+		return str;
 	
+	while (str[i])
+	{
+		while (str[i + y] == to_find[y])
+		{
+			y++;
+			if (!to_find[y])
+				return (&str[i]);
+		}
+		i++;
+	}
+	return (0);
 }
