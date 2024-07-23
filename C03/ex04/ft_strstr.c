b7555f7def9ddd46c299e4a28e3dfc3f1c910aec
Aldo <aldo-panzano@hotmail.it>
2024-07-23 18:39:12 +0200

    fix bug, reset y var

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:10:36 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/23 15:36:15 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/23 18:38:29 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ char	*ft_strstr(char *str, char *to_find)
 			if (!to_find[y])
 				return (&str[i]);
 		}
+		y = 0;
 		i++;
 	}
 	return (0);
