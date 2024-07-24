43c1c41625e6e78797cf01b13ecbb3f963601ce8
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-24 12:31:09 +0200

    fix norm error

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 10:43:51 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/23 18:33:12 by aldo             ###   ########.fr       */
+/*   Updated: 2024/07/24 12:30:52 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcat(char *dest, char *src)
 {
 	char	*tdest;
+	
 	tdest = dest;
 	while (*tdest)
 	{
