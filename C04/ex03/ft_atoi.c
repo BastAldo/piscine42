1444fe6c3467453e3d654f9998d0378f97e7cba1
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-24 10:46:41 +0200

    add ex03

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 11:46:13 by aldpanza          #+#    #+#             */
-/*   Updated: 2024/07/19 12:53:49 by aldpanza         ###   ########.fr       */
+/*   Updated: 2024/07/24 10:46:23 by aldpanza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,27 @@
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	pos;
 	int	sign;
-	int	stop;
 	int	number;
 
 	number = 0;
 	sign = 1;
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if (str[i] != ' ')
+		if (str[i] != ' ' && (!((str[i] >= 9) && (str[i] <= 13))))
 		{
-			if (str[i] == '-' && str[i] != '+')
-			{
-				sign *= - sign;
-			}
+			if (str[i] == '-')
+				sign = sign * -1;
 			else if (str[i] >= '0' && str[i] <= '9')
 			{
-				number *= 10 + (str[i] - '0');
+				number = number * 10 + (str[i] - '0');
 			}
 			else
-			{
-				return (number);
-			}
+				if (number)
+					return (number * sign);
 		}
 		i++;
 	}
-	return (number);
+	return (number * sign);
 }
-
-int	main(void)
-{
-	ft_atoi("       ----+-+-+-+--0125543560adf");
-	return (0);
-}
-
