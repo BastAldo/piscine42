e8d7a5e3a54b2a829dde3e9c2c20cdb63b4799d5
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-24 12:25:11 +0200

    add ex02

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 12:10:46 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/24 12:24:49 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0 || nb == 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb -1));
+}
