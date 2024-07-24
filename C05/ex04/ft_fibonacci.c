f46fc4bce48dc768272e5312f7b269ce973f0ab9
Aldo <aldo-panzano@hotmail.it>
2024-07-24 22:28:12 +0200

    fix ex 04

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,17 +6,18 @@
 /*   By: aldo <aldo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 19:48:41 by aldo              #+#    #+#             */
-/*   Updated: 2024/07/24 21:37:54 by aldo             ###   ########.fr       */
+/*   Updated: 2024/07/24 22:27:35 by aldo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TOD: fail test ->  Fibonacci of 5 Expected 5, got 1
 int	ft_fibonacci(int index)
 {
 	if (index == 0)
 		return (0);
 	else if (index == 1 || index == 2)
 		return (1);
+	else if (index < 0)
+		return (-1);
 	else
 		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
