eaa3b34d3ddf8de19117f9fdefe4a9e688374a60
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-25 10:29:24 +0200

    add ex07

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 09:42:06 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/25 10:29:06 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb <= 0)
+		return (0);
+	if (nb == 1)
+		return (1);
+	while (i < (nb / 2))
+	{
+		if ((i * i) == nb)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_is_prime(int nb)
+{
+	int	c;
+
+	c = 5;
+	if (nb < 2)
+		return (0);
+	if (nb >= 2 && nb <= 3)
+		return (1);
+	else if (nb % 2 == 0)
+		return (0);
+	else if (nb % 3 == 0)
+		return (0);
+	while (c <= ft_sqrt(nb))
+	{
+		if ((nb % c) == 0)
+			return (0);
+		c += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+	{
+		return (2);
+	}
+	while (!ft_is_prime(nb))
+	{
+		nb++;
+	}
+	return (nb);
+}
