fa089c41ca3ad61f9132fa6d4070f377a158608a
Aldo Panzano <aldpanza@e3r3p8.42roma.it>
2024-07-26 11:58:17 +0200

    add ex00

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldpanza <aldpanza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 10:51:56 by aldpanza          #+#    #+#             */
+/*   Updated: 2024/07/26 11:57:35 by aldpanza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char		*dest;
+	long int	i;
+	//long int	y;
+
+	i = 0;
+	//y = 0;
+	while (src[i])
+		i++;
+	dest = (char *)malloc((i+1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+
+	while (i)
+	{
+		dest[i] = src[i];
+		i--;
+	}
+	return (dest);
+}
