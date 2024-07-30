701d30f2bc1bf8855d1759a95e626737b6e4c33f
Aldo <aldo-panzano@hotmail.it>
2024-07-30 23:19:21 +0200

    add ex03

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -12,14 +12,15 @@
 
 #include <stdlib.h>
 
-int	pstrlen(int size, char **strs, char *sep)
+int	ft_pstrlen(int size, char **strs, char *sep)
 {
 	int	strlen;
 	int	i;
 	int	y;
 
-	i = 0;
+	i = 1;
 	y = 0;
+	strlen = 0;
 	while (i < size)
 	{
 		while (strs[i][y])
@@ -35,7 +36,7 @@ int	pstrlen(int size, char **strs, char *sep)
 	{
 		i++;
 	}
-	strlen += (i * size);
+	strlen += (i * (size-2));
 	return (strlen);
 }
 
@@ -44,19 +45,45 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 
 	char	*strjoin;
 	int	strlen;
+	int	i;
+	int	y;
+	int	order;
 
 	if (!size)
 	{
 		return (NULL);
 	}
-	strlen = pstrlen(size, strs, sep);
+	strlen = ft_pstrlen(size, strs, sep) + 1;
 	strjoin = (char *)malloc(strlen * sizeof(char));
-	return (NULL); // remove, just for TS
+	i = 1;
+	y = 0;
+	order = y;
+	while (i < size)
+	{
+		while (strs[i][y])
+		{
+			strjoin[order] = strs[i][y];
+			y++;
+			order++;
+		}
+		y = 0;
+		while (sep[y])
+		{
+			strjoin[order] = sep[i];
+			y++;
+			order++;
+		}
+		y = 0;
+		i++;
+	}
+	strjoin[order] = 0;
+	return (strjoin);
 }
 
-int	main(int argc, char **argv)
-{
-	char	*r;
-	r = ft_strjoin(argc, argv,"--");
-	return (0);
-}
+//int	main(int argc, char **argv)
+//{
+//	char	*r;
+//
+//	r = ft_strjoin(argc, argv,"--");
+//	return (0);
+//}
